3c8f94492a5a168b628fefc3a25d19148aa91d4d
Mariusz Dzikowski <thecyste@gmail.com>
2022-02-08 01:53:22 +0100

    Add components to aliases.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { Module } from '@core/module'
 import { FooSystem } from '@systems/foo.system'
+import { IdentityComponent } from '@components/identity.component'
 
 @Module({
   systems: [FooSystem],
+  components: [IdentityComponent],
 })
 export class AppModule {}
